f6d8b9b1f770cf29ee04fd17cae146ad840090ca
Jericho Sharman <jsharman@hotmail.com.au>
2022-08-06 22:58:35 +1000

    after wsl 2

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { Provider } from "react-redux";
 import App from "./App";
 import store from "./app/store";
 import "antd/dist/antd.css";
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root")); //BUG: HACK: ISSUE: TODO: FIXME: bug: DOME: 
 root.render(
   <React.StrictMode>
     <Router>
